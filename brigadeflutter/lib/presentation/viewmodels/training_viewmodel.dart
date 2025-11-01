759ec637cdc99358633e26df6f7404f7cff1c81c
karenfbc <k.fuentesb@uniandes.edu.co>
2025-10-31 22:50:38 -0500

    feat: caching trainings info (no imagenes)

--- brigadeflutter/lib/presentation/viewmodels/training_viewmodel.dart
+++ brigadeflutter/lib/presentation/viewmodels/training_viewmodel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hive/hive.dart';
 import '../../data/entities/training_card.dart';
 import '../../data/entities/training_progress.dart';
 import '../../data/repositories/training_repository.dart';
@@ -25,7 +26,35 @@ class TrainingViewModel extends ChangeNotifier {
     _status = UiStatus.loading;
     notifyListeners();
 
+    final box = await Hive.openBox('trainingsBox');
+
     try {
+      final cachedCards = box.get('cards');
+      final cachedProgress = box.get('progress');
+
+      if (cachedCards != null && cachedProgress != null) {
+        _cards = (cachedCards as List)
+            .map((c) => TrainingCard(
+                  id: c['id'] as String,
+                  title: c['title'] as String,
+                  subtitle: c['subtitle'] as String,
+                  imageUrl: c['imageUrl'] as String,
+                  cta: c['cta'] as String,
+                ))
+            .toList();
+
+        _progress = (cachedProgress as List)
+            .map((p) => TrainingProgress(
+                  id: p['id'] as String,
+                  label: p['label'] as String,
+                  percent: p['percent'] as int,
+                ))
+            .toList();
+
+        _status = UiStatus.ready;
+        notifyListeners();
+      }
+
       final uid = FirebaseAuth.instance.currentUser?.uid;
       if (uid == null) {
         _status = UiStatus.error;
@@ -33,8 +62,38 @@ class TrainingViewModel extends ChangeNotifier {
         return;
       }
 
-      _cards = await _repo.getCards();
-      _progress = await _repo.getProgress(uid); 
+      final cards = await _repo.getCards();
+      final progress = await _repo.getProgress(uid);
+
+      _cards = cards;
+      _progress = progress;
+
+      await box.put(
+        'cards',
+        cards
+            .map((c) => {
+                  'id': c.id,
+                  'title': c.title,
+                  'subtitle': c.subtitle,
+                  'imageUrl': c.imageUrl,
+                  'cta': c.cta,
+                })
+            .toList(),
+      );
+
+      await box.put(
+        'progress',
+        progress
+            .map((p) => {
+                  'id': p.id,
+                  'label': p.label,
+                  'percent': p.percent,
+                })
+            .toList(),
+      );
+
+      await box.put('lastUpdated', DateTime.now().toIso8601String());
+
       _status = UiStatus.ready;
     } catch (e, st) {
       debugPrint('Training load error: $e\n$st');
@@ -51,9 +110,22 @@ class TrainingViewModel extends ChangeNotifier {
     _submitting = true;
     notifyListeners();
 
+    final box = await Hive.openBox('trainingsBox');
+
     try {
-      await _repo.start(uid, cardId); 
-      _progress = await _repo.getProgress(uid); 
+      await _repo.start(uid, cardId);
+      _progress = await _repo.getProgress(uid);
+
+      await box.put(
+        'progress',
+        _progress
+            .map((p) => {
+                  'id': p.id,
+                  'label': p.label,
+                  'percent': p.percent,
+                })
+            .toList(),
+      );
     } catch (e, st) {
       debugPrint('Training start error: $e\n$st');
     } finally {
