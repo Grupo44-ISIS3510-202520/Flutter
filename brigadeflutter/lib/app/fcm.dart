94136821e57bfd925b684dd3f0ef29baffba7202
jennn-arce <88865588+jennn-arce@users.noreply.github.com>
2025-11-03 15:55:59 -0500

    Comment out debug print statements in FCM setup

--- brigadeflutter/lib/app/fcm.dart
+++ brigadeflutter/lib/app/fcm.dart
@@ -9,22 +9,22 @@ Future<void> setupFCM() async {
     badge: true,
     sound: true,
   );
-  print('User granted permission: ${settings.authorizationStatus}');
+  //print('User granted permission: ${settings.authorizationStatus}');
 
   // get FCM token
   String? token = await messaging.getToken();
-  print('FCM Token: $token');
+  //print('FCM Token: $token');
 
   // listen for foreground messages
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    print('Received foreground message: ${message.notification?.title}');
+    //print('Received foreground message: ${message.notification?.title}');
     if (message.notification != null) {
-      print('Message also contained a notification: ${message.notification}');
+      //print('Message also contained a notification: ${message.notification}');
     }
   });
 
   // handle messages when user taps notification and app is opened from background
   FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-    print('Message opened app: ${message.data}');
+    //print('Message opened app: ${message.data}');
   });
 }
